c508f7d440a6a6aa4fba831a6af427ff15f8fe8d
Rahma-Aly <71734575+Rahma-Aly@users.noreply.github.com>
2021-04-19 17:04:54 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node
+typedef struct 
 {
 char std_name[30];
 int id;
 int bd[3];
 int score;
-struct node* next;
-} Node;
+} student;
 
 int main()
 {
 printf("\twelcome\nteam members:Janna Sallam Rahma Aly Zahrah walid");
-Node* n_ptr;
 return 0;
 }
